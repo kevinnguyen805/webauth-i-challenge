2f94f47fd0c72c39d4b1e36567f44c6b0862ed9b
admin <admin@admins-MacBook-Pro.local>
2019-11-11 11:38:20 -0800

    users and auth structure set up

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,14 +1,9 @@
-/* 
-TODO: Complete the following endpoints
-* POST - /register
-* POST - /login
-* GET - /users 
-
-*/
 const bcrypt = require('bcryptjs');
 const express = require('express')
 const router = express.Router();
 
+
+const authRouter = 
 
 
 
--- api/server.js
+++ api/server.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const apiRouter = require('./apiRouter.js')
-const helmet = require('helmet')
 const configureMiddleware = require('./configure-middleware.js')
 
 const server = express();
 configureMiddleware(server);
 
 
-server.use(helmet());
-server.use(express.json())
 server.use('/api', apiRouter);
 
 server.get('/', (req, res) => {

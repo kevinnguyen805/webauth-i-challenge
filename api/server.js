c00cfe210dcd128270c9a759e36a6b550380d09d
admin <admin@admins-MacBook-Pro.local>
2019-11-12 11:55:17 -0800

    installed sessions and sessionConfiguration

--- api/server.js
+++ api/server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const configureMiddleware = require('./configureMiddleware.js')
 const apiRouter = require('./apiRouter.js')
+const session = require('express-session')
 
 const server = express();
 configureMiddleware(server);
 
+const sessionConfiguration = {
+     name: "kevin",
+     secret: process.env.COOKIE_SECRET || 'is it secret? is it safe?',
+     cookie:{
+          maxAge: 1000 * 60 * 60 * 10, 
+          secure: process.env.NODE_ENV === 'development' ? false : true,
+          httpOnly: true
+     },
+     resave: false, 
+     saveUninitialized: true
+}
+
+server.use(session(sessionConfiguration))
 server.use('/api', apiRouter);
 server.get('/', (req, res) => {
-     res.send('Hello, you are at the root / of the api!')
+     res.json({session: req.session})
 })
 
 module.exports = server;
--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -3,24 +3,37 @@ const db = require('../users/userModel.js')
 
 // ENDPOINT AUTHENTICATION
 module.exports = (req, res, next) => {
-     const { username, password } = req.headers
-
-     if (username && password){
-          db.findBy(username)
-          .first()              // * YOU NEED TO SAY FIRST TO TAKE IT OUT OF THE ARRAY!
-          .then(response => {
-               if(response && bcrypt.compareSync(password, response.password)){
-                    next();
-               } else{
-                    res.status(401).json({message: "Invalid credentials"})
-               }
-          })
-          .catch(error => {
-               res.status(500).json({message: "Unable to retrieve data. Please try again later."})
-          })
-     } else {
-          res.status(401).json({message: "Please provide valid credentials"})
+     if(req.session && req.session.username){
+          
      }
+
 }
+
+
+// module.exports = (req, res, next) => {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     db.findBy(username)
+//       .first() // * YOU NEED TO SAY FIRST TO TAKE IT OUT OF THE ARRAY!
+//       .then(response => {
+//         if (response && bcrypt.compareSync(password, response.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "Invalid credentials" });
+//         }
+//       })
+//       .catch(error => {
+//         res
+//           .status(500)
+//           .json({
+//             message: "Unable to retrieve data. Please try again later."
+//           });
+//       });
+//   } else {
+//     res.status(401).json({ message: "Please provide valid credentials" });
+//   }
+// };
+
 
 
